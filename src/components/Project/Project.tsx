bce3440498d1c40a07c7de34eb130ca12555f3bb
ras00786 <rasool84658@gmail.com>
2024-03-28 14:08:44 +0530

    add project

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -42,15 +42,15 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
-                <a href="https://github.com/ras00786/Coustomer-Survey-Form" target="_blank" rel="noreferrer">
+                <a href="https://github.com/ras00786/TodoListReact" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" />
                 </a>
               </div>
             </header>
             <div className="body">
-              <h3>Customer Survey Form</h3>
+              <h3>Todo List</h3>
               <p>
-              This is a customer survey form. It is a form that helps customers to fill out their details. This form help client to know their user deatails.
+              In this project, I developed a full-stack TodoList application using React for the frontend and Node.js with Express for the backend. The application allows users to manage their tasks efficiently, including adding new tasks, removing completed tasks, and marking tasks as completed. With a clean and intuitive user interface, users can easily organize their daily activities and stay productive.
               </p>
             </div>
             <footer>
@@ -66,9 +66,9 @@ export function Project() {
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
-              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
+              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title>you</svg>
               <div className="project-links">
-                <a href="https://justmedicalbooks.com/" target="_blank" rel="noreferrer">
+                <a href="https://green-beach-0caa2f210.3.azurestaticapps.net/sign-in?redirectUrl=/company-list" target="_blank" rel="noreferrer">
                   <img src={externalLink} alt="Visit site" />
                 </a>
               </div>
